62b751c282e836e947bac1ceac04b5525d4e2dfb
Mathieu Marques <mathieu.marques@manomano.com>
2022-02-17 11:34:02 +0100

    Minor clean up and add outline headings

--- modules/rational-ui.el
+++ modules/rational-ui.el
@@ -1,17 +1,21 @@
 ;;; rational-ui.el -*- lexical-binding: t; -*-
 
+(straight-use-package 'all-the-icons)
+(straight-use-package 'doom-modeline)
+(straight-use-package 'doom-themes)
+(straight-use-package 'elisp-demos)
+(straight-use-package 'helpful)
+
+;;;; Font
+
 (defcustom rational-ui-default-font nil
-  "The configuration of the `default' face, uses a plist with
-the same key names as accepted by `set-face-attribute'")
+  "The configuration of the `default' face.
+Use a plist with the same key names as accepted by `set-face-attribute'.")
 
 (when rational-ui-default-font
   (apply 'set-face-attribute 'default nil (cons :font rational-ui-default-font)))
 
-(straight-use-package 'all-the-icons)
-(straight-use-package 'doom-modeline)
-(straight-use-package 'doom-themes)
-(straight-use-package 'helpful)
-(straight-use-package 'elisp-demos)
+;;;; Mode-Line
 
 ;; Start up the modeline after initialization is finished
 (add-hook 'after-init-hook 'doom-modeline-init)
@@ -22,7 +26,9 @@ the same key names as accepted by `set-face-attribute'")
 (customize-set-variable 'doom-modeline-minor-modes t)
 (customize-set-variable 'doom-modeline-buffer-file-name-style 'truncate-except-project)
 
-;; Make `describe-*' screens more helpful!
+;;;; Help Buffers
+
+;; Make `describe-*' screens more helpful
 (require 'helpful)
 (global-set-key [remap describe-function] #'helpful-callable)
 (global-set-key [remap describe-variable] #'helpful-variable)
@@ -30,17 +36,20 @@ the same key names as accepted by `set-face-attribute'")
 (global-set-key (kbd "C-h F") #'helpful-function)
 (global-set-key (kbd "C-h C") #'helpful-command)
 
+;;;; Elisp-Demos
+
 ;; also add some examples
 (require 'elisp-demos)
-(advice-add 'helpful-update :after
-#'elisp-demos-advice-helpful-update)
+(advice-add 'helpful-update :after #'elisp-demos-advice-helpful-update)
+
 ;; add visual pulse when changing focus, like beacon but built-in
 ;; from from https://karthinks.com/software/batteries-included-with-emacs/
 (defun pulse-line (&rest _)
   "Pulse the current line."
   (pulse-momentary-highlight-one-line (point)))
+
 (dolist (command '(scroll-up-command scroll-down-command
-				     recenter-top-bottom other-window))
+				                     recenter-top-bottom other-window))
   (advice-add command :after #'pulse-line))
 
 (provide 'rational-ui)
