ab1b93f39e3bba8b91806b15931433c82095b6a1
Alejandro Barocio A <abarocio80@gmail.com>
2022-02-20 09:38:51 -0600

    Starting configuration for project management

---
+++ modules/rational-project.el
@@ -0,0 +1,19 @@
+;;;; rational-project.el --- Starting configuration for project management  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+;; SPDX-License-Identifier: MIT
+
+;; Author: System Crafters Community
+
+;;; Commentary
+
+;;
+
+;;; Code:
+
+(let ((user-projects (expand-file-name "projects" rational-config-path)))
+(when (file-exists-p user-projects)
+  (setq project-list-file user-projects)))
+
+(provide 'rational-project)
+;;; rational-project.el ends here
