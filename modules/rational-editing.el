1f6fe17a6c909764126848957e1f04b95316b5a9
Jobayed Islam <96297162+jiploading@users.noreply.github.com>
2022-02-20 02:23:43 +0000

    Highlight current line in prog mode

--- modules/rational-editing.el
+++ modules/rational-editing.el
@@ -21,9 +21,12 @@
 ;; Set a global binding for better line commenting/uncommenting
 (global-set-key (kbd "M-/") 'evilnc-comment-or-uncomment-lines)
 
-;; parentheses
+;; Parentheses
 (electric-pair-mode 1) ; auto-insert matching bracket
 (show-paren-mode 1)    ; turn on paren match highlighting
+
+;; Highlight current line
+(add-hook 'prog-mode-hook 'hl-line-mode)
 
 (provide 'rational-editing)
 ;;; rational-editing.el ends here
