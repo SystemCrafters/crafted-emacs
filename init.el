98b9be6b8369e9e5972044bc069552e77fec52d9
Anders Jackson <anders.jackson@hig.se>
2022-02-26 14:52:04 +0100

    (init.el) Use Emacs (emacs-init-time) to get init time.
    Use the built in Emacs function to calculate the execution time to
    start Emacs.

--- init.el
+++ init.el
@@ -4,9 +4,7 @@
 (add-hook 'emacs-startup-hook
           (lambda ()
             (message "Rational Emacs loaded in %s."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time))))))
+                     (format "%.2f seconds" (emacs-init-time)))))
 
 ;; Add the modules folder to the load path
 (add-to-list 'load-path (expand-file-name "modules/" user-emacs-directory))
