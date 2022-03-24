60ff669119f36802bce49eac3480a7b9923b76c1
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-03-23 18:56:07 -0600

    Relocated cape and corfu support to rational-completion.el

--- modules/rational-completion.el
+++ modules/rational-completion.el
@@ -19,6 +19,8 @@
 (straight-use-package 'orderless)
 (straight-use-package 'marginalia)
 (straight-use-package 'embark)
+(straight-use-package 'corfu)
+(straight-use-package 'cape)
 
 (defun rational-completion/minibuffer-backward-kill (arg)
   "When minibuffer is completing a file name delete up to parent
@@ -75,6 +77,58 @@ folder, otherwise delete a word"
 
 ;; Use Embark to show bindings in a key prefix with `C-h`
 (setq prefix-help-command #'embark-prefix-help-command)
+
+;;;; Corfu
+
+;; Setup corfu for popup like completion
+(require 'corfu)
+
+(defcustom rational-code-completion-ycm-style nil
+  "Make corfu act closer to YouCompleteMe of Vim fame."
+  :type 'boolean)
+
+(when rational-code-completion-ycm-style
+  (setq corfu-quit-at-boundary nil
+        corfu-preselect-first nil)
+  (define-key corfu-map (kbd "RET") nil) ;; Don't overwrite the enter key
+  (define-key corfu-map (kbd "S-RET") 'corfu-insert))
+
+(setq
+  corfu-cycle t                    ; Allows cycling through candidates
+  corfu-auto t                     ; Enable auto completion
+  corfu-auto-prefix 2              ; Complete with less prefix keys
+  corfu-auto-delay 0.0             ; No delay for completion
+  corfu-echo-documentation 0.25    ; Echo docs for current completion option
+  )
+
+(define-key corfu-map (kbd "TAB") 'corfu-next)
+(define-key corfu-map [tab] 'corfu-next)
+(define-key corfu-map (kbd "S-TAB") 'corfu-previous)
+(define-key corfu-map [backtab] 'corfu-previous)
+
+(corfu-global-mode 1)
+
+;;;; Cape
+
+;; Setup Cape for better completion-at-point support and more
+(require 'cape)
+
+;; Add useful defaults completion sources from cape
+(add-to-list 'completion-at-point-functions #'cape-file)
+(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+
+;; Silence the pcomplete capf, no errors or messages!
+;; Important for corfu
+(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+
+;; Ensure that pcomplete does not write to the buffer
+;; and behaves as a pure `completion-at-point-function'.
+(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+(add-hook 'eshell-mode-hook
+          (lambda () (setq-local corfu-quit-at-boundary t
+                            corfu-quit-no-match t
+                            corfu-auto nil)
+            (corfu-mode)))
 
 (provide 'rational-completion)
 ;;; rational-completion.el ends here
