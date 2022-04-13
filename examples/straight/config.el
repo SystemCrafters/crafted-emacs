5dbc3efc39a668e4e02898c2f441d38e668db665
Jeff Bowman <jeff.t.bowman@gmail.com>
2022-04-13 11:50:41 -0500

    Add example configs (#125)
    * Add examples directory for various config examples

---
+++ examples/straight/config.el
@@ -0,0 +1,17 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(defmacro rational-package-install-package (package)
+  "Install PACKAGE using straight"
+  `(straight-use-package ,package))
+
---
+++ examples/straight/early-config.el
@@ -0,0 +1,1 @@
+(setq package-enable-at-startup nil)
