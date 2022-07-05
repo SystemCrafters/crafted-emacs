293c5bef8dfc0c804025a287e5f2d3ccc3ea064b
saccarosium <github.e41mv@aleeas.com>
2022-07-05 23:54:21 +0200

    Add to rational default some sane behavior of tmp files

--- modules/rational-defaults.el
+++ modules/rational-defaults.el
@@ -11,6 +11,36 @@
 
 ;;; Code:
 
+;; Defining variable for XDG directories specifications
+(defvar xdg-data-home-directory
+  (if (getenv "XDG_DATA_HOME")
+    (getenv "XDG_DATA_HOME")
+    (expand-file-name ".local/share/")))
+
+(defvar xdg-cache-home-directory
+  (if (getenv "XDG_DATA_HOME")
+    (getenv "XDG_DATA_HOME")
+    (expand-file-name ".cache/")))
+
+;; Keep emacs folder clean
+
+;; Backup file ( init.el~ )
+(customize-set-variable backup-directory-alist `(("." . ,(expand-file-name "emacs/backups/" xdg-data-home-directory))))
+;; Emacs autosave files ( #init.el# )
+;; The autosave feature doesn't create the directory
+(make-directory (expand-file-name "emacs/auto-save/" xdg-cache-home-directory) t)
+(customize-set-variable auto-save-file-name-transforms
+    `((".*" ,(expand-file-name "emacs/auto-save/" xdg-cache-home-directory) t)))
+
+;; eshell history, lastdir and aliases
+(customize-set-variable eshell-directory-name 
+  (expand-file-name "emacs/eshell" xdg-data-home-directory))
+(customize-set-variable eshell-aliases-file 
+  (expand-file-name "emacs/eshell/aliases" xdg-data-home-directory))
+;; bookmark default file
+(customize-set-variable bookmark-default-file 
+  (expand-file-name "emacs/bookmark-default.el" xdg-data-home-directory))
+
 ;; Revert Dired and other buffers
 (customize-set-variable 'global-auto-revert-non-file-buffers t)
 
@@ -18,7 +48,7 @@
 (global-auto-revert-mode 1)
 
 ;; Use spaces instead of tabs
-(setq-default indent-tabs-mode nil)
+(customize-set-variable-default indent-tabs-mode nil)
 
 ;; Use "y" and "n" to confirm/negate prompt instead of "yes" and "no"
 ;; Using `advice' here to make it easy to reverse in custom
@@ -27,7 +57,7 @@
 ;; N.B. Emacs 28 has a variable for using short answers, which should
 ;; be preferred if using that version or higher.
 (if (boundp 'use-short-answers)
-    (setq use-short-answers t)
+    (customize-set-variable use-short-answers t)
   (advice-add 'yes-or-no-p :override #'y-or-n-p))
 
 ;; Turn on recentf mode
@@ -39,15 +69,15 @@
 (customize-set-variable 'kill-do-not-save-duplicates t)
 
 ;; Make scrolling less stuttered
-(setq auto-window-vscroll nil)
+(customize-set-variable auto-window-vscroll nil)
 (customize-set-variable 'fast-but-imprecise-scrolling t)
 (customize-set-variable 'scroll-conservatively 101)
 (customize-set-variable 'scroll-margin 0)
 (customize-set-variable 'scroll-preserve-screen-position t)
 
 ;; Better support for files with long lines
-(setq-default bidi-paragraph-direction 'left-to-right)
-(setq-default bidi-inhibit-bpa t)
+(customize-set-variable-default bidi-paragraph-direction 'left-to-right)
+(customize-set-variable-default bidi-inhibit-bpa t)
 (global-so-long-mode 1)
 
 ;; Make shebang (#!) file executable when saved
