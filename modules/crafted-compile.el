d018ea88a9e2254b842604e914bfd2c5bbe39f32
Alejandro Barocio A <abarocio80@gmail.com>
2022-07-13 14:08:21 -0500

    Changing name from `rational` to `crafted` following #194

--- modules/crafted-compile.el
+++ modules/crafted-compile.el
@@ -1,4 +1,4 @@
-;;;; rational-compile.el --- Autocompile emacs-lisp code  -*- lexical-binding: t; -*-
+;;;; crafted-compile.el --- Autocompile emacs-lisp code  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2022
 ;; SPDX-License-Identifier: MIT
@@ -12,7 +12,7 @@
 ;; automatic compilation of some emacs-lisp source files.
 
 ;; This could be activated for the `modules' subdirectory under your
-;; `user-emacs-directory', your files under `rational-config-path', or
+;; `user-emacs-directory', your files under `crafted-config-path', or
 ;; all files with mode `emacs-lisp-mode' after saving the file.
 
 ;; It also chooses to compile to byte-code or native code depending if
@@ -24,48 +24,48 @@
 
 ;;;; Variables:
 ;; We define a variable to enable/disable modules compilation.
-(defvar rational-compile-modules t
+(defvar crafted-compile-modules t
   "When non-nil, compile emacs lisp sources for the modules on startup.")
 
 ;; We define a variable to enable/disable user's modules compilation.
-(defvar rational-compile-user-modules t
+(defvar crafted-compile-user-modules t
   "When non-nil, compile user's emacs lisp sources for the modules on startup.")
 
-(defvar rational-compile-module-list '()
+(defvar crafted-compile-module-list '()
   "A list of modules to compile.")
 
-(defvar rational-compile-modules-path
-  `(,(expand-file-name "custom-modules" rational-config-path)
+(defvar crafted-compile-modules-path
+  `(,(expand-file-name "custom-modules" crafted-config-path)
     ,(expand-file-name "modules" user-emacs-directory))
   "Path where to locate modules.")
 
-(defvar rational-compile-user-configuration t
+(defvar crafted-compile-user-configuration t
   "When non-nil, compile emacs lisp sources for the modules on startup.")
 
-(defvar rational-compile-init-files nil
+(defvar crafted-compile-init-files nil
   "When non-nil, compile init files on startup.")
 
-(defvar rational-compile-on-save nil
+(defvar crafted-compile-on-save nil
   "When non-nil, compile the file after saving it.")
 
-(defvar rational-compile-extra-directories-list nil
+(defvar crafted-compile-extra-directories-list nil
   "List of extra directories to search for source-files.
 
 This could be another directory added to `load-path', that is not
-standard, nor is it part of rational-emacs.")
+standard, nor is it part of crafted-emacs.")
 
-(defvar rational-compile-init-files-list
+(defvar crafted-compile-init-files-list
   '("early-init.el" "init.el")
   "List of initialization file names.")
 
-(defvar rational-compile-config-files-list
+(defvar crafted-compile-config-files-list
   '("early-config.el" "config.el")
   "List of configuration file names.")
 
 
 ;;;; Functions:
 ;; A function to locate a module's source file:
-(defun rational-locate-module-file (module &optional path)
+(defun crafted-locate-module-file (module &optional path)
   "Locates the source-file for the MODULE.
 
 MODULE must be a symbol.
@@ -73,8 +73,8 @@ MODULE must be a symbol.
 If the optional argument PATH is non-nil, it asumes it is a list
 containing the name of the directories to search for the
 source-file.  If it is ommited or nil, it will search in the
-directories defined in `rational-compile-modules-path' and
-`rational-compile-extra-directories-list'.
+directories defined in `crafted-compile-modules-path' and
+`crafted-compile-extra-directories-list'.
 
 It returns the first source-file that matches for the MODULE in
 the order specified search path.  If it finds none, it retorns
@@ -82,13 +82,13 @@ nil."
   (let ((dir (seq-find (lambda (dir)
                (file-exists-p (expand-file-name (format "%s.el" (symbol-name module)) dir)))
              (or path
-                 (flatten-list `(,rational-compile-modules-path
-                                 ,rational-compile-extra-directories-list))))))
+                 (flatten-list `(,crafted-compile-modules-path
+                                 ,crafted-compile-extra-directories-list))))))
     (when dir
       (expand-file-name (format "%s.el" (symbol-name module)) dir))))
 
 ;; A function to compile a specific file
-(defun rational-compile-file (f)
+(defun crafted-compile-file (f)
   "Compiles (native or byte-code) the file F.
 
 F could be a single file or a list of files.
@@ -100,7 +100,7 @@ directory will be compiled, but not it's subdirectories."
   (if (featurep 'native-compile)
     (dolist (source f)
       (if (file-newer-than-file-p
-             (rational-compile-locate-eln-file (file-name-base source))
+             (crafted-compile-locate-eln-file (file-name-base source))
              source)
           (native-compile-async f)
         (message "Skipping compilation of file %s" source)))
@@ -111,17 +111,17 @@ directory will be compiled, but not it's subdirectories."
           (byte-recompile-file source nil 0))))))
 
 ;; A function to compile the buffer's file
-(defun rational-compile-buffer (&optional b)
+(defun crafted-compile-buffer (&optional b)
   "Compiles (native or byte-code) the file of buffer B."
   (when (and b ;; Let's be sure it is not nil
              (not (bufferp b)))
     (cl-return nil))
   (let ((file (buffer-file-name b)))
     (when file
-      (rational-compile-file file))))
+      (crafted-compile-file file))))
 
 ;; A function to compile a specific directory
-(defun rational-compile-directory (d)
+(defun crafted-compile-directory (d)
   "Compiles (native or byte-ocde) the files within directory D.
 
 D could be a single directory or a list of directories."
@@ -133,118 +133,118 @@ D could be a single directory or a list of directories."
       (byte-recompile-directory source 0))))
 
 ;; A function to get the list of modules directories with full path:
-(defun rational-compile--config-dirs-list ()
+(defun crafted-compile--config-dirs-list ()
   "Returns a list of configured directories to autocompile."
-  (flatten-list `(,(and rational-compile-modules
+  (flatten-list `(,(and crafted-compile-modules
                         (expand-file-name "modules/" user-emacs-directory))
-                  ,(and rational-compile-user-modules
-                        (expand-file-name "custom-modules/" rational-config-path))
-                  ,rational-compile-extra-directories-list)))
+                  ,(and crafted-compile-user-modules
+                        (expand-file-name "custom-modules/" crafted-config-path))
+                  ,crafted-compile-extra-directories-list)))
 
 ;; A function to get the list of init files with full path:
-(defun rational-compile--init-files-list ()
+(defun crafted-compile--init-files-list ()
   "Returns a list of the init files."
   (mapcar (lambda (f)
             (expand-file-name f user-emacs-directory))
-          rational-compile-init-files-list))
+          crafted-compile-init-files-list))
 
-(defun rational-compile--config-files-list ()
+(defun crafted-compile--config-files-list ()
   "Returns a list of the init files."
   (mapcar (lambda (f)
-            (expand-file-name f rational-config-path))
-          rational-compile-config-files-list))
-;;(rational-compile--config-files-list)
+            (expand-file-name f crafted-config-path))
+          crafted-compile-config-files-list))
+;;(crafted-compile--config-files-list)
 
 ;; A function to execute the compilation of the modules within the list:
-(defun rational-compile-modules ()
-  "Compile specific files defined in `rational-compile-module-list'.
+(defun crafted-compile-modules ()
+  "Compile specific files defined in `crafted-compile-module-list'.
 
-If `rational-compile-init-files' is non-nil, then it compiles
-the init-files (`rational-compile-init-files-list') within
+If `crafted-compile-init-files' is non-nil, then it compiles
+the init-files (`crafted-compile-init-files-list') within
 `user-emacs-directory'.
 
-If `rational-compile-user-configuration' is non-nil, then it
-compiles the config-files (`rational-compile-config-files-list')
-within `rational-config-path'.
+If `crafted-compile-user-configuration' is non-nil, then it
+compiles the config-files (`crafted-compile-config-files-list')
+within `crafted-config-path'.
 
-If `rational-compile-modules' is non-nil, then it compiles all
+If `crafted-compile-modules' is non-nil, then it compiles all
 the source-files for the modules defined in
-`rational-compile-module-list' within the subdirectory 'modules'
+`crafted-compile-module-list' within the subdirectory 'modules'
 in `user-emacs-directory', or in any directory specified in
-`rational-compile-extra-directories-list'.
+`crafted-compile-extra-directories-list'.
 
-If `rational-compile-user-modules' is non-nil, then it compiles
+If `crafted-compile-user-modules' is non-nil, then it compiles
 all the source-files for the modules defined in
-`rational-compile-module-list' within the subdirectory 'modules'
-in `rational-config-path'.
+`crafted-compile-module-list' within the subdirectory 'modules'
+in `crafted-config-path'.
 
 If any source-file for any module specified in
-`rational-compile-module-list' doesn't exist within the paths
+`crafted-compile-module-list' doesn't exist within the paths
 specified, then it is ignored without a warning."
   (interactive)
-  (when rational-compile-init-files
-    (rational-compile-init))
-  (when rational-compile-user-configuration
-    (rational-compile-config))
-  (when rational-compile-modules
-    (dolist (module rational-compile-module-list)
-      (let ((module-src (rational-locate-module-file module
+  (when crafted-compile-init-files
+    (crafted-compile-init))
+  (when crafted-compile-user-configuration
+    (crafted-compile-config))
+  (when crafted-compile-modules
+    (dolist (module crafted-compile-module-list)
+      (let ((module-src (crafted-locate-module-file module
                                                      (flatten-list
                                                       `(,(expand-file-name "modules/" user-emacs-directory)
-                                                        ,rational-compile-extra-directories-list)))))
+                                                        ,crafted-compile-extra-directories-list)))))
         (when module-src
-          (rational-compile-file module-src)))))
-  (when rational-compile-user-modules
-    (dolist (module rational-compile-module-list)
-      (let ((module-src (rational-locate-module-file module
-                                                     `(,(expand-file-name "custom-modules/" rational-config-path)))))
+          (crafted-compile-file module-src)))))
+  (when crafted-compile-user-modules
+    (dolist (module crafted-compile-module-list)
+      (let ((module-src (crafted-locate-module-file module
+                                                     `(,(expand-file-name "custom-modules/" crafted-config-path)))))
         (when module-src
-          (rational-compile-file module-src))))))
+          (crafted-compile-file module-src))))))
 
 ;; A funciton to execute the compilation of the init files:
-(defun rational-compile-init ()
+(defun crafted-compile-init ()
   "Compile (native or byte-code) the initialization files.
 
 The files to be compiled is defined in
-`rational-compile-init-files-list'."
+`crafted-compile-init-files-list'."
   (interactive)
-  (rational-compile-file (rational-compile--init-files-list)))
+  (crafted-compile-file (crafted-compile--init-files-list)))
 
 ;; A funciton to execute the compilation of the config files:
-(defun rational-compile-config ()
+(defun crafted-compile-config ()
   "Compile (native or byte-code) the configuration files.
 
 The files to be compiled is defined in
-`rational-compile-config-files-list'."
+`crafted-compile-config-files-list'."
   (interactive)
-  (rational-compile-file (rational-compile--config-files-list)))
+  (crafted-compile-file (crafted-compile--config-files-list)))
 
 
 ;;;; Hooks:
 
 ;;;;; Modules
 ;; To autocompile modules.  This could be toggled by setting
-;; `rational-compile-modules' or
-;; `rational-compile-user-configuration' either to `nil' or
+;; `crafted-compile-modules' or
+;; `crafted-compile-user-configuration' either to `nil' or
 ;; `t'.
 (add-hook 'emacs-startup-hook ;; or kill-emacs-hook?
           (lambda ()
-            (when (or rational-compile-modules
-                      rational-compile-user-modules
-                      rational-compile-init-files
-                      rational-compile-user-configuration)
-              (rational-compile-modules))))
+            (when (or crafted-compile-modules
+                      crafted-compile-user-modules
+                      crafted-compile-init-files
+                      crafted-compile-user-configuration)
+              (crafted-compile-modules))))
 
 ;;;;; On save files
 ;; To auto compile after saving the file.  This could be toggled by
-;; seting `rational-compile-on-save' to `nil' or `t'
+;; seting `crafted-compile-on-save' to `nil' or `t'
 (add-hook 'after-save-hook
           (lambda ()
-            (when (and rational-compile-on-save
+            (when (and crafted-compile-on-save
                        (string-equal major-mode "emacs-lisp-mode"))
-              (rational-compile-buffer))))
+              (crafted-compile-buffer))))
 
-(defun rational-compile-locate-eln-file (library)
+(defun crafted-compile-locate-eln-file (library)
   ""
   (let ((return nil))
     (dolist (dir (mapcar (lambda (path)
@@ -263,5 +263,5 @@ The files to be compiled is defined in
 
 
 ;;; Package:
-(provide 'rational-compile)
-;;; rational-compile.el ends here
+(provide 'crafted-compile)
+;;; crafted-compile.el ends here
