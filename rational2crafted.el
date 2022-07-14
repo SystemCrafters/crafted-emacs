70345fbfbb3f4c825db6b546470090294ebc2558
Jeff Bowman <jeff.t.bowman@gmail.com>
2022-07-14 14:03:30 -0500

    Refactor to defuns to make this more usable by chemacs2 users

--- rational2crafted.el
+++ rational2crafted.el
@@ -37,30 +37,16 @@
 ;; the same way the `early-init.el' file does.
 (defvar rational-config-path
   (cond
-   ((featurep 'chemacs)
-    (if (getenv  "RATIONAL_EMACS_HOME")
-        (expand-file-name (getenv "RATIONAL_EMACS_HOME"))
-      (expand-file-name "rational-emacs" user-emacs-directory)))
-   ((getenv "RATIONAL_EMACS_HOME") (expand-file-name (getenv "RATIONAL_EMACS_HOME")))
    ((or (file-exists-p (xdg-config-home))
         (file-exists-p (expand-file-name "rational-emacs" (xdg-config-home))))
     (expand-file-name "rational-emacs" (xdg-config-home)))
    ((getenv "HOME") (expand-file-name ".rational-emacs" (getenv "HOME"))))
   "The user's rational-emacs configuration path.")
 
-(print (concat "rational config path: " rational-config-path))
-
 ;; Set the `crafted-config-path' from the `rational-config-path'
 (defvar crafted-config-path
   (string-replace "rational" "crafted" rational-config-path)
   "The user's crafted-emacs config path.")
-
-(print (concat "crafted config path: " crafted-config-path))
-
-;; Copy the rational-emacs folder to crafted-emacs.  This,
-;; effectively, makes a backup of the rational-config-path. From here,
-;; we will only work with in the crafted-config-path folder.
-(copy-directory rational-config-path crafted-config-path)
 
 (defvar crafted-config-file
   (expand-file-name "config.el" crafted-config-path)
@@ -70,51 +56,74 @@
   (expand-file-name "early-config.el" crafted-config-path)
   "The user's crafted-emacs early init file, early-config.el")
 
-(print (concat "crafted-config-file: " crafted-config-file))
-(print (concat "crafted-early-config-file: " crafted-early-config-file))
-
-(defun perform-replacement ()
+(defun r2c-perform-replacement ()
   (goto-char (point-min))
   (while (re-search-forward "rational" nil t)
     (replace-match "crafted")))
 
-;; Modify the config.el file to replace the occurrences of `rational'
-;; with `crafted'.
-(with-temp-file crafted-config-file
-  (insert-file-contents crafted-config-file)
-  (perform-replacement))
+(defun r2c-transition-file-1 (file)
+  "Transition a file from rational to crafted."
+  (with-temp-file file
+    (insert-file-contents file)
+    (r2c-perform-replacement))
+  (print (concat "modified: " file)))
 
-;; Same with the early-config.el file, if it exists
-(when (file-exists-p crafted-early-config-file)
-  (with-temp-file crafted-early-config-file
-    (insert-file-contents crafted-early-config-file)
-    (perform-replacement)))
+(defun r2c-transition-file-2 (original-file renamed-file)
+  "Transition a file from rational to crafted, renaming on the ways--either
 
-(print (concat "modified: " crafted-config-file))
+ORIGINAL-FILE will be renamed to RENAMED-FILE. ORIGINAL-FILE will be removed."
+  (with-temp-file renamed-file
+    (insert-file-contents original-file)
+    (r2c-perform-replacement))
+  (delete-file original-file)
+  (print (concat "modified: " renamed-file)))
 
-(when (file-exists-p crafted-early-config-file)
-  (print (concat "modified: " crafted-early-config-file)))
+(defun r2c-main ()
+  (print (concat "rational config path: " rational-config-path))
+  (print (concat "crafted config path: " crafted-config-path))
 
-;; Update any custom modules that may exist. Replace occurrences of
-;; `rational' with `crafted' within each file. Take into account the
-;; word `rational' may appear in the file name and this script would
-;; have changed a `require' in the `config.el' to use the word
-;; `crafted' instead, so rename any files with the word `rational' to
-;; have the word `crafted' instead.
-(let ((files (mapcar (lambda (f)        ; get a list of custom module file names, fully path qualified
-                       (let ((p (expand-file-name "custom-modules" crafted-config-path)))
-                         (expand-file-name f p)))
-                     (seq-drop (directory-files (expand-file-name "custom-modules" crafted-config-path)) 2))))
-  (let ((crafted-file-names (mapcar (lambda (f) (string-replace "rational" "crafted" f)) ; crafted file names
-                                    files)))
-    (seq-do-indexed (lambda (file-name i)
-                      (let ((crafted-name (seq-elt crafted-file-names i)))
-                        (with-temp-file crafted-name ; create the crafted version of the file
-                          (insert-file-contents file-name) ; insert the contents of the rational
-                          (perform-replacement))
-                        (print (concat "modified: " crafted-name))
-                        (delete-file file-name))) ; delete the rational file
-                    files)))
+  ;; Copy the rational-emacs folder to crafted-emacs.  This,
+  ;; effectively, makes a backup of the rational-config-path. From here,
+  ;; we will only work with in the crafted-config-path folder.
+  (copy-directory rational-config-path crafted-config-path)
+
+  (print (concat "crafted-config-file: " crafted-config-file))
+  (print (concat "crafted-early-config-file: " crafted-early-config-file))
+
+  ;; Modify the config.el file to replace the occurrences of `rational'
+  ;; with `crafted'.
+  (r2c-transition-file-1 crafted-config-file)
+
+  ;; Same with the early-config.el file, if it exists
+  (when (file-exists-p crafted-early-config-file)
+    (r2c-transition-file-1 crafted-early-config-file))
+
+  ;; Update any custom modules that may exist. Replace occurrences of
+  ;; `rational' with `crafted' within each file. Take into account the
+  ;; word `rational' may appear in the file name and this script would
+  ;; have changed a `require' in the `config.el' to use the word
+  ;; `crafted' instead, so rename any files with the word `rational' to
+  ;; have the word `crafted' instead.
+  (when (file-exists-p (expand-file-name "custom-modules" crafted-config-path))
+      (let ((files (mapcar (lambda (f)        ; get a list of custom module file names, fully path qualified
+                         (let ((p (expand-file-name "custom-modules" crafted-config-path)))
+                           (expand-file-name f p)))
+                       (seq-drop (directory-files (expand-file-name "custom-modules" crafted-config-path)) 2))))
+    (let ((crafted-file-names (mapcar (lambda (f) (string-replace "rational" "crafted" f)) ; crafted file names
+                                      files)))
+      (seq-do-indexed (lambda (file-name i)
+                        (let ((crafted-name (seq-elt crafted-file-names i)))
+                          (r2c-transition-file-2 file-name crafted-name)))
+                      files)))))
+(when (or (featurep 'chemacs)
+          (file-exists-p (expand-file-name "~/.emacs-profiles.el")))
+  (print "Migration for chemacs based profiles is not supported.")
+  (print "However, you may refer to the script for the forms needed")
+  (print "to craft your own migration."))
+
+(unless (or (featurep 'chemacs)
+          (file-exists-p (expand-file-name "~/.emacs-profiles.el")))
+  (r2c-main))
 
 (provide 'rational2crafted)
 ;;; rational2crafted.el ends here
