b59b4826f90b03d8e2bcaf0f9879b662878f67ae
wereket <dagnachewa@gmail.com>
2022-07-24 21:02:11 -0400

    crafted-emacs

---
+++ modules/crafted-elm.el
@@ -0,0 +1,18 @@
+;;;; crafted-elm.el --- elm configuration            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+;; SPDX-License-Identifier: MIT
+
+;; Author: System Crafters Community
+
+;;; Commentary:
+
+;; elm configuration
+(crafted-package-install-package 'elm-mode)
+
+(setq elm-format-on-save t)
+
+;;; Code:
+
+(provide 'crafted-elm)
+;;; crafted-elm.el ends here
