4932667496c3029f7424530faa5b1aff772bfeaa
Jeff Bowman <jeff.t.bowman@gmail.com>
2023-02-01 21:49:45 +0000

    Renamed early-init and init as example files

---
+++ early-init-example.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- Emacs early initialization for Crafted Emacs (optional) -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;;; Code:
+
+;; Configures `package.el'
+(load "~/crafted-emacs/modules/crafted-early-init-config")
+
+(provide 'early-init)
+
+;;; early-init.el ends here
