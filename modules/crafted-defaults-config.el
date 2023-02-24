1ad7cc30446f34effb21c1abd62827fe9a9b4d98
Jeff Bowman <jeff.t.bowman@gmail.com>
2023-02-24 14:40:48 -0600

    Remove customization for use-short-answers
    The docstring for use-short-answers does not recommend setting it to a
    non-nil value as the intention is to have the user think about what
    they are doing rather than answering too quickly.  Crafted Emacs will
    support this position, the user can customize this value in their own
    configuration if they choose.

--- modules/crafted-defaults-config.el
+++ modules/crafted-defaults-config.el
@@ -24,16 +24,6 @@
 ;; Use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 
-;; Use "y" and "n" to confirm/negate prompt instead of "yes" and "no"
-;; Using `advice' here to make it easy to reverse in custom
-;; configurations with `(advice-remove 'yes-or-no-p #'y-or-n-p)'
-;;
-;; N.B. Emacs 28 has a variable for using short answers, which should
-;; be preferred if using that version or higher.
-(if (boundp 'use-short-answers)
-    (setq use-short-answers t)
-  (advice-add 'yes-or-no-p :override #'y-or-n-p))
-
 ;; Turn on recentf mode
 (add-hook 'after-init-hook #'recentf-mode)
 
