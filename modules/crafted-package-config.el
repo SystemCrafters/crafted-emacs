558602bf9c5903e719c6ff7356d233c808348b3d
Jeff Bowman <jeff.t.bowman@gmail.com>
2023-02-24 13:29:48 -0600

    Use System Crafters header.
    The standard auto-insert template got used when I created this, should
    have used the system crafters module template instead.

--- modules/crafted-package-config.el
+++ modules/crafted-package-config.el
@@ -1,22 +1,9 @@
 ;;; crafted-package-config.el --- Macro helpers for crafted modules  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  GNU Guix Live
+;; Copyright (C) 2022
+;; SPDX-License-Identifier: MIT
 
-;; Author: GNU Guix Live <guest@gnu>
-;; Keywords: lisp
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; Author: System Crafters Community
 
 ;;; Commentary:
 
