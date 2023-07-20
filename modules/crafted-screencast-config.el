c32c9adaef3aa0b92b30fb3b3dbbd7be10baf7ac
jvdydev <intincDev@googlemail.com>
2023-07-20 08:12:26 +0200

    Fix keycast-mode being renamed to keycast-mode-line-mode
    As outlined in issue #262, the keycast package renamed it's default mode
    from `keycast-mode` to `keycast-mode-line-mode`.

--- modules/crafted-screencast-config.el
+++ modules/crafted-screencast-config.el
@@ -14,7 +14,7 @@
 (when (locate-library "keycast")
   (customize-set-variable 'keycast-remove-tail-elements nil)
   (customize-set-variable 'keycast-insert-after 'mode-line-misc-info)
-  (keycast-mode))
+  (keycast-mode-line-mode))
 
 (provide 'crafted-screencast-config)
 ;;; crafted-screencast.el ends here
