d6619e51b689f865e8722612df4180de8ec00dfb
jvdydev <intincDev@googlemail.com>
2023-07-29 23:05:12 +0200

    Fix hook to enhance electric-pair-inhibit-predicate
    Introduces named function "crafted-org/enhance-electric-pair-inhibit-predicate"
    to disable auto-pairing of < in org-mode.
    
    This previously failed if electric-pair-mode was not enabled before org-mode.
    The new checks (and it being added to both org-mode-hook and
    electric-pair-mode-hook) properly function no matter when electric-pair-mode is
    enabled in regards to org-mode files.

--- modules/crafted-org-config.el
+++ modules/crafted-org-config.el
@@ -26,11 +26,21 @@
 (when (featurep 'org-appear)
   (add-hook 'org-mode-hook 'org-appear-mode))
 
-;; Disable auto-pairing of "<" in org-mode
-(add-hook 'org-mode-hook (lambda ()
-                           (setq-local electric-pair-inhibit-predicate
-                                       `(lambda (c)
-                                          (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+;; Disable auto-pairing of "<" in org-mode with electric-pair-mode
+(defun crafted-org/enhance-electric-pair-inhibit-predicate ()
+  "Disable auto-pairing of \"<\" in `org-mode' when using `electric-pair-mode'."
+  (when (and electric-pair-mode (eql major-mode #'org-mode))
+    (setq-local electric-pair-inhibit-predicate
+                `(lambda (c)
+                   (if (char-equal c ?<)
+                       t
+                     (,electric-pair-inhibit-predicate c))))))
+
+;; Add hook to both electric-pair-mode-hook and org-mode-hook
+;; This ensures org-mode buffers don't behave weirdly,
+;; no matter when electric-pair-mode is activated.
+(add-hook 'electric-pair-mode-hook #'crafted-org/enhance-electric-pair-inhibit-predicate)
+(add-hook 'org-mode-hook #'crafted-org/enhance-electric-pair-inhibit-predicate)
 
 (provide 'crafted-org-config)
 ;;; crafted-org-config.el ends here
