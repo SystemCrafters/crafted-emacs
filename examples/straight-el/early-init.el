fc127204194e44d6b1e86395f384f260a3f27e4a
Jeffrey 'jf' Lim <jf@users.noreply.github.com>
2024-06-09 22:17:22 +0800

    examples/straight-el/early-init.el: update straight.el bootstrap code (bootstrap-version 7)

--- examples/straight-el/early-init.el
+++ examples/straight-el/early-init.el
@@ -19,9 +19,11 @@
 ;; See https://github.com/radian-software/straight.el#getting-started
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el"
-                         user-emacs-directory))
-      (bootstrap-version 6))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
